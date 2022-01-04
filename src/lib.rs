ac80fea19cc00dea7d2c054e742e9af0db8bb454
Martin Seon <hyunmui@outlook.kr>
2022-01-05 08:36:26 +0900

    continue trait...

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+use std::fmt::{format, Display};
+
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("{}님의 기사 더 읽기", self.summarize_author())
+    }
 }
 
 pub struct NewsArticle {
@@ -10,8 +16,8 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {}, ({})", self.headline, self.author, self.location)
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
     }
 }
 
@@ -23,7 +29,34 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify(item: impl Summary) {
+    println!("속보! {}", item.summarize())
+}
+
+pub fn notify2<T: Summary>(item1: T, item2: T) {
+    todo!()
+}
+
+pub fn notify3_1(item: impl Summary + Display) {
+    todo!()
+}
+
+pub fn notify3_2<T: Summary + Display>(item: T) {
+    todo!()
+}
+
+fn return_summarizable() -> impl Summary {
+    // 한가지의 객체종류만 리턴 가능함. 조건절로 분기 안됨
+    // 454page 참조 필요
+    Tweet {
+        username: String::from("abcd"),
+        content: String::from("eeeee"),
+        reply: false,
+        retweet: false,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_trait::{Tweet, Summary};
+use rust_trait::{NewsArticle, Summary, Tweet};
 
 fn main() {
     let tweet = Tweet {
@@ -9,4 +9,16 @@ fn main() {
     };
 
     println!("One of New tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("대한민국, 러시아 월드컵 예선에서 독일을 이겼다."),
+        location: String::from("카잔 아레나, 러시아"),
+        author: String::from("위키백과"),
+        content: String::from(
+            "2018년 6월 27일 러시아 카잔의 카잔 아레나에서 열린 
+            2018 월드컵 F조 3차전 경기에서 대한민국이 독일에 2:0 승리를 거뒀다.",
+        ),
+    };
+
+    println!("New Article: {}", article.summarize());
 }
