5c6faa06e10a2bca0983873e17bb8fca2020345c
Martin Seon <hyunmui@outlook.kr>
2022-01-05 08:56:50 +0900

    10-3 complete

--- src/lib.rs
+++ src/lib.rs
@@ -60,3 +60,28 @@ fn return_summarizable() -> impl Summary {
         retweet: false,
     }
 }
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+/**
+ * PartialOrd -> 비교 가능 Trait
+ * Display -> ToString 을 통한 출력 가능
+ */
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("Largest Member x: {}", self.x);
+        } else {
+            println!("Largest Member y: {}", self.y);
+        }
+    }
+}
